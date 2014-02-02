5c807813f49ec6ba65ed87cbd4daf8244ff74ca0
valerie <valerie.douglas@openlogic.com>
2014-02-02 14:54:28 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grapefruit::Application.config.secret_token = '873e1c1b8ce66e98afa3bd7d103aa04e1e2f6341802aed4edc70420b02c66c82d11e39fcb190aa0bd2b944c38d3f8cca46850446ff75881198a2ae7982069054'
